d8f12b2051882fc407a352e052b2169c7471508f
Bogie <chagchen@outlook.com>
2020-06-03 16:44:49 -0700

    edited index.js and updated ssr package.json

--- examples/ssr/index.js
+++ examples/ssr/index.js
@@ -1,6 +1,25 @@
 const React = require('react')
-const ReactCssDemoSpinners = require('react-css-demo-spinners')
 const express = require('express')
+const ReactDOM = require('react-dom/server')
+const { Ripple } = require('react-css-demo-spinners')
 
-
+const port = 3001
 const app = express()
+
+app.get('*', (req, res) => {
+  const html = ReactDOM.renderToString(React.createElement(Ripple))
+
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+    </head>
+    <body>
+      ${html}
+    </body>
+    </html>
+  `)
+})
+
+app.listen(port, () => console.log(`http://localhost:${port}`))
